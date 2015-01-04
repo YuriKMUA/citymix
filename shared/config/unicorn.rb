05c2def82e1ee91aac0299d9a067e131a476b0c3
YuriKMUA <yurikmua@gmail.com>
2015-01-04 13:14:10 +0200

    Finish

--- shared/config/unicorn.rb
+++ shared/config/unicorn.rb
@@ -1,9 +1,9 @@
 # Рабочие директории приложения на сервере
-root       = '/var/www/yurikmua/data/citymix.com.ua'
-rails_root = "/var/www/yurikmua/data/citymix.com.ua/current"
+root       = '/var/www/yurikmua/data/www/citymix.com.ua'
+rails_root = "/var/www/yurikmua/data/www/citymix.com.ua/current"
 
 # Файлы, хранящие идентификаторы запущенных Unicorn-процессов
-pidfile     = "/var/www/yurikmua/data/citymix.com.ua/shared/run/unicorn.pid"
+pidfile     = "/var/www/yurikmua/data/www/citymix.com.ua/shared/run/unicorn.pid"
 pidfile_old = pidfile + '.oldbin'
 pid pidfile
 
@@ -13,18 +13,18 @@ preload_app true
 timeout 30
 
 # Путь к сокету
-listen "/var/www/yurikmua/data/citymix.com.ua/shared/run/unicorn.sock", :backlog => 1024
+listen "/var/www/yurikmua/data/www/citymix.com.ua/shared/run/unicorn.sock", :backlog => 1024
 
 #Путь к лог-файлам
-stderr_path "/var/www/yurikmua/data/citymix.com.ua/current/log/unicorn_error.log"
-stdout_path "/var/www/yurikmua/data/citymix.com.ua/current/log/unicorn.log"
+stderr_path "/var/www/yurikmua/data/www/citymix.com.ua/current/log/unicorn_error.log"
+stdout_path "/var/www/yurikmua/data/www/citymix.com.ua/current/log/unicorn.log"
 
 #Установки сборщика мусора
 GC.copy_on_write_friendly = true if GC.respond_to?(:copy_on_write_friendly=)
 
 #Блок инструкций, выполняемых до запуска сервера
 before_exec do |server|
-    ENV["BUNDLE_GEMFILE"] = "/var/www/yurikmua/data/citymix.com.ua/current/Gemfile"
+    ENV["BUNDLE_GEMFILE"] = "/var/www/yurikmua/data/www/citymix.com.ua/current/Gemfile"
 end
 
 #Инструкции для управления воркерами и состоянием соединения с БД
